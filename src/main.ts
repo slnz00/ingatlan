f1f48e4fb1c3eeb4721c284acd776265b5fc4aae
slnz00 <balintbakcsa@gmail.com>
2024-07-14 10:36:06 +0200

    config name filter, improve error handling

--- src/main.ts
+++ src/main.ts
@@ -5,37 +5,44 @@ import State from 'state'
 import Template from 'template'
 
 async function main() {
-  await esm.load();
+  await esm.load()
 
-  const { default: open } = esm.modules.open;
-
+  const { default: open } = esm.modules.open
+  const nameFilter = process.argv.slice(2);
   const state = State.instance
 
   await state.initialize()
 
   await Promise.all(
     Object.entries(state.configs).map(async ([name, config]) => {
-      if (config.enabled === false) {
-        console.log(`[${name}] Config is disabled, skipping...`)
+      try {
+        if (nameFilter.length && !nameFilter.includes(name)) {
+          return
+        }
+        if (config.enabled === false) {
+          console.log(`[${name}] Config is disabled, skipping...`)
 
-        return
-      }
+          return
+        }
 
-      const scraper = new Scraper(name, config);
-      const template = new Template(name)
+        const scraper = new Scraper(name, config);
+        const template = new Template(name)
 
-      const startedAt = new Date();
-      const newApartments = await scraper.scrapeNewApartments()
-      const timeElapsed = new Date().getTime() - startedAt.getTime()
+        const startedAt = new Date();
+        const newApartments = await scraper.scrapeNewApartments()
+        const timeElapsed = new Date().getTime() - startedAt.getTime()
 
-      if (newApartments.length) {
-        const htmlPath = await template.saveResultsAsHtml(newApartments)
+        if (newApartments.length) {
+          const htmlPath = await template.saveResultsAsHtml(newApartments)
 
-        console.log(`[${name}] Scraped ${newApartments.length} new apartments (took: ${timeElapsed} ms), opening browser...`)
+          console.log(`[${name}] Scraped ${newApartments.length} new apartments (took: ${timeElapsed} ms), opening browser...`)
 
-        await open(htmlPath).catch(err => console.error(`[${name}] Failed to open browser:`, err))
-      } else {
-        console.log(`[${name}] Could not find new apartments`)
+          await open(htmlPath).catch(err => console.error(`[${name}] Failed to open browser:`, err))
+        } else {
+          console.log(`[${name}] Could not find new apartments`)
+        }
+      } catch (err) {
+        console.error(`[${name}]`, err);
       }
     })
   )
